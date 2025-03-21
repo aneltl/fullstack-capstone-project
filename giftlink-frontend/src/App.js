d957b20d997176c31d1a22220df17f1e9f3c8423
Anel <ledezma.anel@gmail.com>
2025-03-21 13:47:58 -0400

    Add register and login pages

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Removed useNavigate
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
@@ -12,10 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
       </Routes>
     </>
   );
 }
 
 export default App;
-
